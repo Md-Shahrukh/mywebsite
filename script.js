1e339f5b042e9310cd0ec7d5f7446d941b82c208
Md-Shahrukh <shahrukh.md1995@gmail.com>
2023-11-30 23:00:11 +0530

    javascript added

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Example: Change text color when the page loads
+    const paragraph = document.querySelector('p');
+    paragraph.style.color = 'blue';
+});
